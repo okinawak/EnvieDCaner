59cebe0c702c26b4c96c3272257c0095126ba62f
Louis_Belibio <38444276+okinawak@users.noreply.github.com>
2025-06-24 06:05:32 +0200

    Delete fip1.microsoft-store-front directory

--- fip1.microsoft-store-front/src/data/games.js
+++
@@ -1,7 +0,0 @@
-export const games = [
-    { id: 1, title: "Halo Infinite", description: "FPS futuriste.", image: "https://via.placeholder.com/150" },
-    { id: 2, title: "Forza Horizon 5", description: "Course en monde ouvert.", image: "https://via.placeholder.com/150" },
-    { id: 3, title: "Sea of Thieves", description: "Aventure pirate multijoueur.", image: "https://via.placeholder.com/150" },
-    { id: 4, title: "Microsoft Flight Simulator", description: "Simulation de vol réaliste.", image: "https://via.placeholder.com/150" },
-  ];
-  
